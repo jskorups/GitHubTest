1d6286e942359425d831a1928f88e182e462f1c3
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-12-05 10:49:28 +0100

    05122019 #mod 3

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -13,6 +13,7 @@ namespace GitTest
             Console.WriteLine("Baza 1");
             Console.WriteLine("Modyfikacja 1");
             Console.WriteLine("Modyfikacja 2");
+            Console.WriteLine("Modyfikacja 3");
 
             Console.ReadKey();
         }
